7e9857ed4c2cf267b7184ce6053f6da4bc630137
michaeldel <michael.delpech@outlook.com>
2018-02-26 02:12:22 +0100

    Create basic setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='CryptoCompare',
+    version='0.1',
+    packages=['cryptocompare'],
+    license='MIT',
+    long_description=open('README.rst').read(),
+)
